2603c45350534602469faa075940b35be286f496
Rose Momanyi <rosebmomanyi@gmail.com>
2025-03-06 16:58:02 +0300

    Continuation of functions

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // code your solution here
+function saturdayFun (activity= 'roller-skate') {
+    return (`This Saturday, I want to ${activity}!`)
+}
+saturdayFun ()
+
+const mondayWork = function (activity ="go to the office") {
+    return (`This Monday, I will ${activity}.`)
+
+}
+mondayWork('work from home')
+
+function wrapAdjective (flair = "*") {
+    return function (something = "special") {
+        return `You are ${flair}${something}${flair}!`
+
+    }
+}
